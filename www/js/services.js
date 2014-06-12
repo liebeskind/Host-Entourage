32b4e39837a98ae2ec6f92c892ee208c0616e96f
liebeskind <daniel.liebeskind@gmail.com>
2014-06-12 12:16:23 -0700

    Improves styling and adds a few static parties

--- www/js/services.js
+++ www/js/services.js
@@ -28,7 +28,12 @@ angular.module('starter.services', [])
 .factory('Hosted', function() {
   var hosted = [
     { id: 0, name: 'Top Of The Hill June Party', attendees: 53, address: '1902 Leavenworth, San Francisco, CA', type: 'party', theme: 'British', imgUrl: 'russianhillalcatraz.jpeg' },
-    { id: 1, name: 'Fort Mason Party', attendees: 36, address: 'Fort Mason, San Francisco, CA', type: 'barbeque', theme: 'Cowboy', imgUrl: 'ftmason.jpeg' }
+    { id: 1, name: 'Fort Mason Party', attendees: 36, address: 'Fort Mason, San Francisco, CA', type: 'barbeque', theme: 'Cowboy', imgUrl: 'ftmason.jpeg' },
+    { id: 2, name: 'Top Of The Hill May Party', attendees: 23, address: '1902 Leavenworth, San Francisco, CA', type: 'party', theme: 'British', imgUrl: 'russianhillalcatraz.jpeg' },
+    { id: 3, name: 'Top Of The Hill April Party', attendees: 23, address: '1902 Leavenworth, San Francisco, CA', type: 'party', theme: 'British', imgUrl: 'russianhillalcatraz.jpeg' },
+    { id: 4, name: 'Top Of The Hill March Party', attendees: 23, address: '1902 Leavenworth, San Francisco, CA', type: 'party', theme: 'British', imgUrl: 'russianhillalcatraz.jpeg' },
+    { id: 5, name: 'Top Of The Hill Feb Party', attendees: 23, address: '1902 Leavenworth, San Francisco, CA', type: 'party', theme: 'British', imgUrl: 'russianhillalcatraz.jpeg' },
+    { id: 6, name: 'Top Of The Hill Jan Party', attendees: 23, address: '1902 Leavenworth, San Francisco, CA', type: 'party', theme: 'British', imgUrl: 'russianhillalcatraz.jpeg' }
   ];
 
   return {
