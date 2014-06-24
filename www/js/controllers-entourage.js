46c0c5c9dd1ab7340a2994ef76327935f78b3978
liebeskind <daniel.liebeskind@gmail.com>
2014-06-24 14:48:43 -0700

    Changes PendingEntourages to MyEntourages and adds lock property

--- www/js/controllers-entourage.js
+++ www/js/controllers-entourage.js
@@ -1,7 +1,7 @@
 angular.module('entourage.controllers', [])
 
-.controller('CreateEntourageCtrl', function($scope, $location, PendingEntourages) {
-  var recent = PendingEntourages.get(0)
+.controller('CreateEntourageCtrl', function($scope, $location, MyEntourages) {
+  var recent = MyEntourages.get(0)
   $scope.entourage = {}
   $scope.entourage.name = recent.name;
   
@@ -16,15 +16,15 @@ angular.module('entourage.controllers', [])
       newEntourage[i] = {name: newArray[i], accepted: false};
     };
     $location.path('/main/entourage/createentourage2');
-    PendingEntourages.addNewEntourage(newEntourage);
+    MyEntourages.addNewEntourage(newEntourage);
   };
 
   $scope.addEntourage = function(entourage) {
-    PendingEntourages.createEntourage(entourage);
+    MyEntourages.createEntourage(entourage);
     $location.path("/main/entourage/viewentourages")
   };
 })
 
-.controller('ViewEntouragesCtrl', function($scope, $location, PendingEntourages) {
-	$scope.pendingentourages = PendingEntourages.all();
+.controller('ViewEntouragesCtrl', function($scope, $location, MyEntourages) {
+	$scope.myentourages = MyEntourages.all();
 })
--- www/js/services-entourage.js
+++ www/js/services-entourage.js
@@ -1,8 +1,8 @@
 angular.module('entourage.services', [])
 
-.factory('PendingEntourages', function() {
-  var pendingentourages = [
-    { id: 0, name: "Daniel's Entourage", date: '6/26/14', 
+.factory('MyEntourages', function() {
+  var myentourages = [
+    { id: 0, name: "Daniel's Entourage", date: '6/26/14', locked: false,
       captain: {name: 'Daniel Liebeskind', imgUrl: 'danliebeskind.jpg', facebook: 'https://www.facebook.com/daniel.liebeskind'},
       members: [
         {id: 0, name: 'Danielle Diamond', accepted: true, imgUrl: 'daniellediamond.jpg', facebook: 'https://www.facebook.com/danielle.diamond'}, 
@@ -10,7 +10,7 @@ angular.module('entourage.services', [])
         {id: 2, name: 'Rayna Roumie', accepted: true, imgUrl: 'raynaroumie.jpg', facebook: 'https://www.facebook.com/rayna.roumie'}
       ]
     },
-    { id: 1, name: "My Awesome Entourage", date: '6/27/14', 
+    { id: 1, name: "My Awesome Entourage", date: '6/27/14', locked: false, 
       captain: {name: 'Daniel Liebeskind', imgUrl: 'danliebeskind.jpg', facebook: 'https://www.facebook.com/daniel.liebeskind'},
       members: [
         {id: 0, name: 'Danielle Diamond', accepted: true, imgUrl: 'daniellediamond.jpg', facebook: 'https://www.facebook.com/danielle.diamond'}, 
@@ -22,33 +22,20 @@ angular.module('entourage.services', [])
 
   return {
     all: function() {
-      return pendingentourages;
+      return myentourages;
     },
     get: function(entourageId) {
-      return pendingentourages[entourageId];
+      return myentourages[entourageId];
     },
     addNewEntourage: function(newEntourage) {
-      pendingentourages.push({ id: pendingentourages.length, members: newEntourage, 
+      myentourages.push({ id: myentourages.length, members: newEntourage, 
       	captain: { name: 'Daniel Liebeskind', imgUrl: 'danliebeskind.jpg', facebook: 'https://www.facebook.com/daniel.liebeskind'}
       });
     },
     createEntourage: function(entourage) {
       for (prop in entourage) {
-        pendingentourages[pendingentourages.length-1][prop] = entourage[prop]; 
+        myentourages[myentourages.length-1][prop] = entourage[prop]; 
       }
     }
   }
-})
-
-.factory('PendingEntourages', function() {
-  var pendingentourages = [
-    { id: 0, name: "Daniel's Entourage", date: '6/26/14', 
-      captain: {name: 'Daniel Liebeskind', imgUrl: 'danliebeskind.jpg', facebook: 'https://www.facebook.com/daniel.liebeskind'},
-      members: [
-        {id: 0, name: 'Danielle Diamond', accepted: true, imgUrl: 'daniellediamond.jpg', facebook: 'https://www.facebook.com/danielle.diamond'}, 
-        {id: 1, name: 'Derek Gillaspy', accepted: false, imgUrl: 'derekgillaspy.jpg', facebook: 'https://www.facebook.com/derek.gillaspy'}, 
-        {id: 2, name: 'Rayna Roumie', accepted: true, imgUrl: 'raynaroumie.jpg', facebook: 'https://www.facebook.com/rayna.roumie'}
-      ]
-    }
-  ]
 })
